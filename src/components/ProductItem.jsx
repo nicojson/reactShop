d0846923ad7a5b146c7bfb0ae8ac9d1237cffab0
nico_json <nicolasgodinez.nssg@gmail.com>
2022-05-01 17:33:07 -0500

    [BUG HUNTERS] reto #3 addedToCart TanZng

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,11 +2,13 @@ import React, { useContext } from 'react';
 import '@styles/ProductItem.scss';
 import AppContext from '../context/AppContext';
 import iconAddToCart from '@icons/bt_add_to_cart.svg'
+import iconAddedToCart from '@icons/bt_added_to_cart.svg';
 
 const ProductItem = ({ product }) => {
-  const { addToCart } = useContext(AppContext);
+  const { state, addToCart } = useContext(AppContext);
 
   const handleClick = item => {
+    console.log('incart: ', state.cart.includes(item));
     addToCart(item);
   }
 
@@ -19,7 +21,12 @@ const ProductItem = ({ product }) => {
           <p>{product.title}</p>
         </div>
         <figure onClick={() => handleClick(product)}>
-          <img src={iconAddToCart} alt="" />
+          { state.cart.includes(product) ? 
+          <img className="disable add-to-cart-btn" 
+          src={iconAddedToCart} alt="added To Cart" />
+          : <img className="add-to-cart-btn pointer" 
+          src={iconAddToCart} alt="add To Cart" />
+          }
         </figure>
       </div>
     </div>
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -23,8 +23,9 @@ const MyOrder = () => {
       </div>
       <div className="my-order-content">
         <div>
-        {state.cart.map(product => (
-          <OrderItem product={product} key={`orderItem-${product.id}`} />
+        {state.cart.map((product, index) => (
+          <OrderItem product={product} 
+          key={`orderItem-${product.id}-${index}`} />
         ))}
         </div>
         <div className="order orderTotal">
